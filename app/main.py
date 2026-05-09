fd0fc06be60e3115bca9bb83701a241b454140a3
Wisdom Alawode <alawodewisdom02@gmail.com>
2026-05-10 00:41:05 +0100

    collecting user input

--- app/main.py
+++ app/main.py
@@ -18,3 +18,10 @@
 
 while True:
     print("==========================")
+    print("> Welcome, I am tort your to do list app\n")
+    print("> This is the command center:\n")
+    print("> Press [1] to create new task\n")
+    print("> press [2] to view all tasks\n")
+    print("> press [3] to exit")
+
+    user_input = input(" > Your command : ")
