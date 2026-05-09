0b9cb2669bbe44678942f43bbc93355ed4b46e87
Wisdom Alawode <alawodewisdom02@gmail.com>
2026-05-10 00:33:51 +0100

    creating app template

---
+++ app/main.py
@@ -0,0 +1,20 @@
+#create task
+
+#view all tasks
+
+#view tasks by id
+
+#update task
+
+#delete task
+
+#mark task as complete
+
+#mark task as incomplete
+
+#view all incomplete tasks
+
+#view all complete task
+
+while True:
+    print("==========================")
