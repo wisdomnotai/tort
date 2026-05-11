81649e8efb5b9010072b319fdd47259e08552e8c
Wisdom Alawode <alawodewisdom02@gmail.com>
2026-05-11 16:13:26 +0100

    creating add task api

--- app/app.py
+++ app/app.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import main
 
 app = FastAPI()
+
+#declaring input input schema
+
+class TaskInput(BaseModel):
+    title: str
+
+#creating path operations for all commands
+
+@app.post("/task/create")
+def create_task():
+    """creating new task"""
+    return main.add_task()
 
