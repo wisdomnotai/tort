6d503da9a0b244e9bd6dbda487a3f2e907f23a9c
Wisdom Alawode <alawodewisdom02@gmail.com>
2026-05-11 16:25:45 +0100

    creating api to mark task complete

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
 import main
 
@@ -21,4 +21,11 @@ def get_task():
     """viewing all tasks"""
     return main.view_all_tasks
 
+@app.patch("/task/{task_id}/complete")
+def update_task(task_id: int):
+    """marking task complete by id"""
+    task = main.complete_task(task_id)
+    if not task:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"{task_id} not found")
+    return task
 
