14bd95e90a1f2f3899eeeb49eb04c97230b14a21
Wisdom Alawode <alawodewisdom02@gmail.com>
2026-05-11 15:59:28 +0100

    pushing version 1 of code logic

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 #creating the array for temproray memory
 todos =  []
 #initalizing id to 0
-id_counter = 0
+id_counter = 1
 
 #create task
 def add_task(title: str):
@@ -13,6 +13,7 @@ def add_task(title: str):
     }
     todos.append(task)
     id_counter += 1
+    print("task succesfully added")
     return task
 #view all tasks
 def view_all_tasks():
@@ -22,7 +23,7 @@ def view_all_tasks():
 def completed_tasks(task_id: int):
     for task in todos:
         if task["id"] == task_id:
-            task["completed"] == True
+            task["completed"] = True
             return task
     return None
 
@@ -32,7 +33,7 @@ def delete_task(task_id: int):
         if task["id"] == task_id:
             todos.remove(task)
             return todos
-        return None 
+    return None 
 
 while True:
     print("==========================")
@@ -40,6 +41,23 @@ while True:
     print("> This is the command center:\n")
     print("> Press [1] to create new task\n")
     print("> press [2] to view all tasks\n")
-    print("> press [3] to exit")
+    print("> press [3] to delete task\n")
+    print("> press[4] to exit\n")
 
-    user_input = input(" > Your command : ")
+    user_input = int(input(" > Your command : "))
+
+    if user_input == 1:
+        task = input("> input task : ")
+        add_task(task)
+    elif user_input == 2:
+        tasks = view_all_tasks()
+        print(tasks)
+    elif user_input == 3:
+        task_id = input("> delete task by id : ")
+        delete_task(task_id)
+    elif user_input == 4:
+        print("app closed")
+        break
+    else:
+        break
+    
